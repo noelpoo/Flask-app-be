da991c6c4496ecd933a01015bc13ac8d38ab2d81
noelpoo <xloel93@hotmail.com>
2021-01-26 15:08:42 +0800

    changed api spec

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from flasgger import Swagger
 
 from security import authenticate, identity
+
+API_VERSION = 'v1'
+API_PATH = '/api/{}'.format(API_VERSION)
 
 app = Flask(__name__)
 app.secret_key = "PALO"
@@ -20,16 +23,22 @@ items = []
 
 class Item(Resource):
 
-    def get(self, name):
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        return {'item': item}, 200 if item is not None else 404
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=True)
+        name = parser.parse_args().get('name')
+        if name:
+            item = next(filter(lambda x: x['name'] == name, items), None)
+            return {'item': item}, 200 if item is not None else 404
+        else:
+            return {'message': "malformed body"}, 400
 
     @jwt_required()
-    def post(self, name):
+    def post(self):
+        request_data = request.get_json(force=True)
+        name = request_data['name']
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {"message": "{} already exists".format(name)}, 400
-
-        request_data = request.get_json(force=True)
         item = {
             'id': len(items) + 1,
             'name': name,
@@ -39,13 +48,17 @@ class Item(Resource):
         return item, 201
 
     @jwt_required()
-    def delete(self, name):
+    def delete(self):
         global items
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=True)
+        name = parser.parse_args().get('name')
         items = list(filter(lambda x: x['name'] != name, items))
         return {'items': items}
 
-    def put(self, name):
+    def put(self):
         request_data = request.get_json()
+        name = request_data['name']
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {
@@ -59,14 +72,28 @@ class Item(Resource):
         return item
 
 
+class ItemID(Resource):
+
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', type=int, required=True)
+        _id = parser.parse_args().get('id')
+        if _id:
+            item = next(filter(lambda x: x['id'] == _id, items), None)
+            return {'item': item}, 200 if item is not None else 404
+        else:
+            return {"message":'malformed request body'}, 400
+
+
 class ItemList(Resource):
 
     def get(self):
         return {'items': items}
 
 
-api.add_resource(Item, '/item/<string:name>')
-api.add_resource(ItemList, '/items')
+api.add_resource(Item, '{}/item'.format(API_PATH))
+api.add_resource(ItemList, '{}/items'.format(API_PATH))
+api.add_resource(ItemID, '{}/item_id'.format(API_PATH))
 
 
 if __name__ == "__main__":
